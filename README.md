4ab732a4bcb2fe1101a35a5e115be3f0b31684f8
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-11-20 03:51:19 +0200

    this is lab7

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cookie-stand
+
+#*This lab talk about Cookie stand* :
+
+he sales for 5 shop in different countries.
+numper of custmors,and the amount of coockies that have sold.
+
+*This project is create by Ala'a Mahasneh*
+
+I used (Google) alot and got help from my instructor and TA's.
+
+
+*including*
+- object construction functions .
+- for loops.
+- console for checking.
+
+
--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict'
 var shops = [];
 var container = document.getElementById("salmon");
-
+var tableEl = document.createElement('table');
+container.appendChild(tableEl);
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 function Shop(location, min, max, avg) {
@@ -13,60 +14,55 @@ function Shop(location, min, max, avg) {
     this.total = 0;
     shops.push(this);
     this.getCookiesPerHour();
-  }
-  Shop.prototype.getRandomCustomer = function() {
+}
+Shop.prototype.getRandomCustomer = function () {
     return Math.ceil(Math.random() * (this.max - this.min + 1)) + this.min;
-  };
-  Shop.prototype.getCookiesPerHour = function() {
+};
+Shop.prototype.getCookiesPerHour = function () {
     for (let i = 0; i < hours.length; i++) {
-      this.numOfCookies.push(Math.floor(this.getRandomCustomer() * this.avg));
-      this.total += this.numOfCookies[i];
+        this.numOfCookies.push(Math.floor(this.getRandomCustomer() * this.avg));
+        this.total += this.numOfCookies[i];
     }
-  };
-  Shop.prototype.render = function() {
-    var tableEl= document.createElement('table');
-    container.appendChild(tableEl);
+};
+Shop.prototype.render = function () {
+    
     var trEl = document.createElement("tr");
     tableEl.appendChild(trEl);
     var tdEl = document.createElement("td");
     trEl.appendChild(tdEl);
     tdEl.textContent = this.name;
     for (var i = 0; i < hours.length; i++) {
-      var tdEl = document.createElement("td");
-      tdEl.textContent = this.numOfCookies[i];
-      trEl.appendChild(tdEl);
+        var tdEl = document.createElement("td");
+        tdEl.textContent = this.numOfCookies[i];
+        trEl.appendChild(tdEl);
     }
     var totalTdEl = document.createElement("td");
     trEl.appendChild(totalTdEl);
     totalTdEl.textContent = this.total;
-  };
-  
-  var seattle = new Shop("Seattle", 23, 65, 6.3);
-  var tokyo = new Shop("Tokyo", 3, 24, 1.2);
-  var dubai = new Shop("Dubai", 11, 38, 3.7);
-  var paris = new Shop("Paris", 20, 38, 2.3);
-  var lima = new Shop("Lima", 2, 16, 4.6);
-  
-  function renderHeader() {
-    var tableEl= document.createElement('table');
-    container.appendChild(tableEl);
+};
+
+var seattle = new Shop("Seattle", 23, 65, 6.3);
+var tokyo = new Shop("Tokyo", 3, 24, 1.2);
+var dubai = new Shop("Dubai", 11, 38, 3.7);
+var paris = new Shop("Paris", 20, 38, 2.3);
+var lima = new Shop("Lima", 2, 16, 4.6);
+
+function renderHeader() {
     var trEl = document.createElement("tr");
     tableEl.appendChild(trEl);
     var thEl = document.createElement("th");
     trEl.appendChild(thEl);
     thEl.textContent = " ";
     for (let i = 0; i < hours.length; i++) {
-      var thEl = document.createElement("th");
-      trEl.appendChild(thEl);
-      thEl.textContent = hours[i];
+        var thEl = document.createElement("th");
+        trEl.appendChild(thEl);
+        thEl.textContent = hours[i];
     }
     var thEl = document.createElement("th");
     trEl.appendChild(thEl);
     thEl.textContent = "Total";
-  }
-  function renderFooter() {
-    var tableEl= document.createElement('table');
-    container.appendChild(tableEl);
+}
+function renderFooter() {
     var trEl = document.createElement("tr");
     tableEl.appendChild(trEl);
     var tdEl = document.createElement("td");
@@ -74,23 +70,22 @@ function Shop(location, min, max, avg) {
     tdEl.textContent = "Total";
     var megaTotal = 0;
     for (let i = 0; i < hours.length; i++) {
-      var sum = 0;
-      for (let j = 0; j < shops.length; j++) {
-        sum += shops[j].numOfCookies[i];
-      }
-      megaTotal += sum;
-      var tdEl = document.createElement("td");
-      trEl.appendChild(tdEl);
-      tdEl.textContent = sum;
+        var sum = 0;
+        for (let j = 0; j < shops.length; j++) {
+            sum += shops[j].numOfCookies[i];
+        }
+        megaTotal += sum;
+        var tdEl = document.createElement("td");
+        trEl.appendChild(tdEl);
+        tdEl.textContent = sum;
     }
     var tdEl = document.createElement("td");
     trEl.appendChild(tdEl);
     tdEl.textContent = megaTotal;
-  }
-  
-  renderHeader();
-  for (let i = 0; i < shops.length; i++) {
+}
+
+renderHeader();
+for (let i = 0; i < shops.length; i++) {
     shops[i].render();
-  }
-  renderFooter();
-  
+}
+renderFooter();
